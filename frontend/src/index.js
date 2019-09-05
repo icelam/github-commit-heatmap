23ca436619aa6a063aff3a8f3f0cfb96e5229146
Ice Lam <pinky.yy.lam@gmail.com>
2019-09-06 02:08:49 +0800

    Fix linter errors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,14 +12,14 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App),
+  render: (h) => h(App)
 }).$mount('#app');
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+    navigator.serviceWorker.register('./service-worker.js').then((registration) => {
       console.log('SW registered: ', registration);
-    }).catch(registrationError => {
+    }).catch((registrationError) => {
       console.log('SW registration failed: ', registrationError);
     });
   });
